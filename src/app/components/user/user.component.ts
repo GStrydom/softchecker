fbb6c3bed822e40b3e65e60886613d66c22eeab7
Greg Strydom <gregory.strydom079@gmail.com>
2022-12-13 08:24:23 +0200

    New auth updates
    Changed new auth

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { UserInterface } from "../../interfaces/user.interface";
 import { UserService } from "../../services/user.service";
-import {Observable} from "rxjs";
+import { Observable } from "rxjs";
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
   selector: 'app-user',
@@ -10,13 +11,14 @@ import {Observable} from "rxjs";
 })
 export class UserComponent implements OnInit {
   public userData
-  @Input() id: string
+  public uid
 
-  constructor(public userService: UserService) { }
+  constructor(public userService: UserService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.userData = this.userService.getUserById(this.id).subscribe((res) => {
-      console.log(`Data: ${res}`);
+    this.uid = this.route.snapshot.url[1].path
+    this.userData = this.userService.getUserById(this.uid).subscribe((res) => {
+      console.log(`Data: ${res}`)
     })
   }
 }
